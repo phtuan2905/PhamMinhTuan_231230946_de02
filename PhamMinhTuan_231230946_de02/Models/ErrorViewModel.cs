1791400880c7a6a838d0f907867842af14acbc35
tuanpham936 <tuan.phamminh9936@gmail.com>
2025-10-24 09:50:44 +0700

    first commit

---
+++ PhamMinhTuan_231230946_de02/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhamMinhTuan_231230946_de02.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
